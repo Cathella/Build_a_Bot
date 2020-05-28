7986baee1c8b8ca29bb50d7e12277db00c128674
Nakitto Catherine <ncathie214@gmail.com>
2020-05-28 20:57:49 +0300

    Add .env file to the project

--- bin/bot.rb
+++ bin/bot.rb
@@ -1,4 +1,6 @@
 require 'telegram_bot'
+require_relative '../lib/bad_code_day.rb'
+
 token = '1188580696:AAEsv6FuEoGg2o7TrhutCKgDVVd770m-VZU'
 bot = TelegramBot.new(token: token)
 
@@ -10,8 +12,11 @@ bot.get_updates(fail_silently: true) do |message|
     when /start/i
       reply.text = "Hello #{message.from.first_name} \nI am your comforter through a bad code day. The aim of the bot is to keep you inspired and motivated at all times. \nRun /inspire command -- To get Motivation Quotes\nRun /music command -- To play Motivation song\n"
     when /inspire/i
-      quotes = ['Keep Strong', 'Keep Calm', 'Keep Moving']
-      reply.text = "#{quotes.sample.capitalize}."
+      quotes = Inspire.new
+      quote = quotes.sample_quote
+      # quotes = ['Keep Strong', 'Keep Calm', 'Keep Moving']
+      # reply.text = "#{quotes.sample.capitalize}."
+      reply.text = "#{quote}"
     when /music/i
       reply.text = 'Your winning song'
     else
---
+++ lib/bad_code_day.rb
@@ -0,0 +1,8 @@
+require 'telegram/bot'
+
+class Inspire
+  quotes = [
+    'Better to light a candle than wasting your time cursing the darkness',
+    'If something is worth doing, it is worth doing right. Do everything you do to the best of your ability'
+  ]
+end
