8c13a1092247d85ec095d5544a7267f83090a736
Nakitto Catherine <ncathie214@gmail.com>
2020-05-28 19:55:27 +0300

    Change bot menu

--- bin/bot.rb
+++ bin/bot.rb
@@ -3,21 +3,17 @@ token = '1188580696:AAEsv6FuEoGg2o7TrhutCKgDVVd770m-VZU'
 bot = TelegramBot.new(token: token)
 
 bot.get_updates(fail_silently: true) do |message|
-  puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
   message.reply do |reply|
     case command
     when /start/i
-      reply.text = "Select a service by running a command\nRun /send command -- To send money\nRun /airtime command -- To buy airtime\nRun /data command -- To buy Internet\nRun /balance command -- To check balance"
-    when /send/i
-      reply.text = 'Enter number to send to'
-    when /airtime/i
-      reply.text = 'Enter number to load to'
-    when /data/i
-      reply.text = 'Enter number to load to'
-    when /balance/i
-      reply.text = 'Enter your number'
+      reply.text = "Hello #{message.from.first_name} \nI am your comforter through a bad code day. The aim of the bot is to keep you inspired and motivated at all times. \nRun /inspire command -- To get Motivation Quotes\nRun /music command -- To play Motivation song\n"
+    when /inspire/i
+      quotes = ['Keep Strong', 'Keep Calm', 'Keep Moving']
+      reply.text = "#{quotes.sample.capitalize}."
+    when /music/i
+      reply.text = 'Your winning song'
     else
       reply.text = "I have no idea what #{command.inspect} means."
     end
